87ba31525ca3b78643f931c530ac860e8862423e
karthik <karthiiii22@gmail.com>
2018-02-05 12:23:36 +0530

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router'
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { EmployeesComponent } from './employees/employees.component';
+
+
+const appRoutes: Routes =  [{ path: 'home', component: HomeComponent },
+                            { path: 'employees', component: EmployeesComponent },
+                            { path: '', redirectTo: '/home', pathMatch: 'full' },
+                            { path: '**', component: PagenotfoundComponent }];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PagenotfoundComponent,
+    EmployeesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
